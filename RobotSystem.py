74413ea7d93a0cd51f1352a336677d3a454bd3cd
Carlos <canutdominguez@gmail.com>
2020-02-11 15:09:09 +0100

    class RobotTransportador implemented but not finished

--- RobotSystem.py
+++ RobotSystem.py
@@ -49,6 +49,7 @@ class Robot:
         self.estado_robot.APAGADO
 # ------------- /Robot class
 
+
 # Robot behaviour for watching over class
 class RobotVigilador(Robot):
 
@@ -63,4 +64,27 @@ class RobotVigilador(Robot):
 
 
 
+# Robot behaviour for transporting objects class
+class RobotTransportador(Robot):
+
+    capacidadMaxCarga = ""
+
+    def __init__(self,id,estado_robot,nivel_bateria,coordenadaX,coordenadaY,capacidadMaxCarga):
+        super().__init__(id,estado_robot,nivel_bateria,coordenadaX,coordenadaY)
+        self.capacidadMaxCarga = capacidadMaxCarga
+
+    # TO-DO: Make use of the Carga class for the methods
+
+    def cogerCarga(self):
+        print("agarro una carga")
+
+    def soltarCarga(self):
+        print("suelto una carga")
+
+    def avanzar(self):
+        print("Avanzo")
+
+    def transportarCarga(self,x,y):
+        print("transporto una carga a x: {}, y: {}".format(x,y))
+# ----------------- /class
 
