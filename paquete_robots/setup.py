9cdaf6933026cca2a7dd76c583b5c13bebfb8b8e
Carlos <canutdominguez@gmail.com>
2020-03-23 19:09:42 +0100

    paquete añadido

---
+++ paquete_robots/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='paquete_robots',
+    version='0.1',
+    description='Paquetes para trabajar con el robot',
+    author='Carlos Maria Canut Dominguez',
+    author_email='canutdominguez@gmail.com',
+    url='https://github.com/CarlosCanut/basic_robot_pyhton_project',
+    packages=['Carga','Enumeraciones','Robot','RobotCirculador','RobotTransportador','RobotVigilador','RobotVolador']
+)
